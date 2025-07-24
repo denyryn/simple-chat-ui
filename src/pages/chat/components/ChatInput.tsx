1f816e52fb3455d45420dbf0119e667f7eb3dae1
Deny Rian <denyryn@outlook.com>
2025-07-24 10:25:52 +0700

    feat: fixed bug in mobile and hidden on null chat

--- src/pages/chat/components/ChatInput.tsx
+++ src/pages/chat/components/ChatInput.tsx
@@ -1,8 +1,13 @@
 import { useRef, useEffect } from "react";
 import Send from "@assets/send.svg";
 import Attachment from "@assets/attachment.svg";
+import type { ChatResult } from "../../../types/chatResult";
 
-export default function ChatInput() {
+export default function ChatInput({
+  currentChat,
+}: {
+  currentChat: ChatResult | null;
+}) {
   const textareaRef = useRef<HTMLTextAreaElement>(null);
 
   const handleInput = () => {
@@ -18,7 +23,11 @@ export default function ChatInput() {
   }, []);
 
   return (
-    <footer className="px-4 py-3 bg-white border-t border-gray-200 rounded-b-xl">
+    <footer
+      className={`px-4 py-3 bg-white border-t border-gray-200 rounded-b-xl ${
+        !currentChat ? "hidden" : ""
+      }`}
+    >
       <div className="flex items-center">
         <button className="p-4">
           <img className="size-6" src={Attachment} alt="Attach" />
@@ -33,7 +42,7 @@ export default function ChatInput() {
             msOverflowStyle: "none", // IE 10+
           }}
           className="flex-1 resize-none overflow-scroll px-3 py-2 rounded-lg placeholder-gray-500
-                     focus:ring-0 focus:outline-none border-none shadow-none max-h-[80px] text-sm text-black"
+                     focus:ring-0 focus:outline-none border-none shadow-none min-h-[40px] max-h-[80px] text-sm text-black"
         />
         <button className="p-4">
           <img className="size-6" src={Send} alt="Send" />
